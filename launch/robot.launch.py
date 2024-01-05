0fcfbd03d5b58926aeb29ed4acc190e83e1f92a0
iam-ironman-2003 <rmuthukumar203at@gmail.com>
2024-01-05 22:18:29 +0530

    created_ros2_control

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -4,8 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription #TimerAction
+from launch.substitutions import Command
 from launch_ros.actions import Node
+#from launch.actions import RegisterEventHandler
+#from launch.event_handlers import OnProcessStart
 
 def generate_launch_description():
  
@@ -20,35 +23,47 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','robot_state_publisher.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
- 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
- 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
+    #delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
     )
+    #delayed_diff_drive_spawner = RegisterEventHandler(
+    #    event_handler=OnProcessStart(
+     #       target_action=controller_manager,
+      #      on_start=[diff_drive_spawner],
+       # )
+    #)
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
     )
+    #delayed_joint_broad_spawner = RegisterEventHandler(
+     #   event_handler=OnProcessStart(
+      #      target_action=controller_manager,
+       #     on_start=[joint_broad_spawner],
+        #)
+    #)
  
     # Launch them all!
     return LaunchDescription([
         robot_state_publisher,
-        gazebo,
-        spawn_entity,
+        controller_manager,
         diff_drive_spawner,
         joint_broad_spawner
     ])
