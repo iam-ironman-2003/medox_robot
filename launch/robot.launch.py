1579227606c0907d4954e5702eff6070bef6eb2f
iam-ironman-2003 <rmuthukumar203at@gmail.com>
2024-02-03 11:42:09 +0530

    rectified serial_port

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -4,11 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription #TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.substitutions import Command
 from launch_ros.actions import Node
-#from launch.actions import RegisterEventHandler
-#from launch.event_handlers import OnProcessStart
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 def generate_launch_description():
  
@@ -21,7 +21,7 @@ def generate_launch_description():
     robot_state_publisher = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','robot_state_publisher.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
@@ -34,36 +34,36 @@ def generate_launch_description():
         parameters=[{'robot_description': robot_description},
                     controller_params_file]
     )
-    #delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
     )
-    #delayed_diff_drive_spawner = RegisterEventHandler(
-    #    event_handler=OnProcessStart(
-     #       target_action=controller_manager,
-      #      on_start=[diff_drive_spawner],
-       # )
-    #)
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
     )
-    #delayed_joint_broad_spawner = RegisterEventHandler(
-     #   event_handler=OnProcessStart(
-      #      target_action=controller_manager,
-       #     on_start=[joint_broad_spawner],
-        #)
-    #)
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
  
     # Launch them all!
     return LaunchDescription([
         robot_state_publisher,
-        controller_manager,
-        diff_drive_spawner,
-        joint_broad_spawner
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
     ])
--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_port': '/dev/ttyUSB0',
                 'frame_id': 'lidar_link',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
